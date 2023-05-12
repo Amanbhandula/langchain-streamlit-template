6ea3f6f1ba2337c11eb3c2a668b12f57f496a6d6
Aman Bhandula <aman@farmako.in>
2023-05-12 19:29:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ from langchain.llms import OpenAI
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
-    prompt = "Act as a mental health chatbot that communicates with users on WhatsApp, asking them relevant questions one at a time, similar to how a psychologist would. Engage in a conversation by asking appropriate questions to understand their mental state, emotions, and concerns, while providing support and empathy."
     llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm, prompt=prompt)
+    chain = ConversationChain(llm=llm, prompt="Act as a mental health chatbot that communicates with users on WhatsApp, asking them relevant questions one at a time, similar to how a psychologist would. Engage in a conversation by asking appropriate questions to understand their mental state, emotions, and concerns, while providing support and empathy.")
     return chain
 
 chain = load_chain()
