0b284682d4f10b1f954bb17476fb4a0837dd6400
Aman Bhandula <aman@farmako.in>
2023-05-12 19:52:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ from langchain.prompts.prompt import PromptTemplate
 template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
 Current conversation:
-{history}
+{memory}
 Human: {input}
 AI Assistant:"""
 PROMPT = PromptTemplate(
-    input_variables={"history": "", "input": ""}, template=template
+    input_variables={"memory": "", "input": ""}, template=template
 )
 
 
