c7a4869e588be861149147685447964f23ac2b86
Aman Bhandula <aman@farmako.in>
2023-05-12 19:43:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ from streamlit_chat import message
 
 from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
+from langchain.prompts.prompt import PromptTemplate
+
+
+
+# Now we can override it and set it to "AI Assistant"
+
+template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+Current conversation:
+{history}
+Human: {input}
+AI Assistant:"""
+PROMPT = PromptTemplate(
+    input_variables=["history", "input"], template=template
+)
+conversation = ConversationChain(
+    prompt=PROMPT,
+    llm=llm, 
+    verbose=True, 
+    memory=ConversationBufferMemory(ai_prefix="AI Assistant")
+)
+
 
 
 def load_chain():
