b9e104fa941372a4a6c5c541d7c8fd1bd4be670c
Aman Bhandula <aman@farmako.in>
2023-07-20 21:05:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """Python file to serve as the frontend"""
-import json
 import streamlit as st
+import json
 from streamlit_chat import message
 
-from langchain.chains import ConversationChain, LLMChain
+from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
 
 def load_chain():
@@ -21,7 +21,6 @@ def load_chain():
     {history}
     Human: {input}
     AI Assistant:"""
-    
     PROMPT = PromptTemplate(
         input_variables=["history", "input"], template=template
     )
@@ -35,30 +34,7 @@ def load_chain():
 
     return chain
 
-def load_history_chain():
-    """Logic for loading the chain that generates a medical history"""
-    from langchain.prompts.prompt import PromptTemplate
-
-    llm = OpenAI(temperature=0)
-
-    # Define the medical history generator prompt
-    template = """
-    You are an AI system that generates random medical history data. Please generate a comprehensive medical history JSON for a random patient.
-    """
-    
-    PROMPT = PromptTemplate(
-        input_variables=[], template=template
-    )
-
-    chain = LLMChain(
-        prompt=PROMPT,
-        llm=llm
-    )
-
-    return chain
-
 chain = load_chain()
-history_chain = load_history_chain()
 
 # From here down is all the StreamLit UI.
 st.set_page_config(page_title="Beta Version", page_icon=":robot:")
@@ -70,26 +46,16 @@ if "generated" not in st.session_state:
 if "past" not in st.session_state:
     st.session_state["past"] = []
 
-if "medical_history" not in st.session_state:
-    st.session_state["medical_history"] = ""
-
 def get_text():
     input_text = st.text_input("You: ", "", key="input")
     return input_text
 
 def get_medical_history():
-    if not st.session_state["medical_history"]:
-        # Generate a new medical history
-        output = history_chain.run(input="")
-        st.session_state["medical_history"] = output
-
-    medical_history = st.session_state["medical_history"]
+    medical_history = st.text_area("Medical History: ", "", key="medical_history")
     return medical_history
 
 user_input = get_text()
 medical_history = get_medical_history()
-
-st.text_area("Medical History: ", value=medical_history, key="medical_history")
 
 if user_input and medical_history:
     combined_input = f"Medical history: {medical_history}. {user_input}"
